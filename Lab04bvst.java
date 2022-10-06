7bbf4d590fafb89dcffc83e8431c93bc15de9ccb
Elise Giroux <elise.giroux@vtsd.com>
2022-10-06 13:55:17 -0400

    hi

--- Lab04bvst.java
+++ Lab04bvst.java
@@ -13,8 +13,13 @@ public class Lab04bvst extends Applet
     public void paint(Graphics g)
     {
         // DRAW CUBE
-
-
+           g.drawLine(25,25,250,25);
+           g.drawLine(250,25,325,150);
+           g.drawLine(25,25, 25,350);
+           g.drawLine(25,25,100,150);
+           g.drawLine(100,150,325,150);
+           g.drawLine(325,150,324,400);
+           g.drawLine
 
         // DRAW SPHERE
 
